b4aea881ce02b62dde5983033cf64f9e6402a6ac
Raghava-Rohan <raghavarohan22@gmail.com>
2024-05-30 15:41:21 +0530

    hms

---
+++ src/main/java/com/codegnan/exception/InvalidPatientIdException.java
@@ -0,0 +1,31 @@
+package com.codegnan.exception;
+
+public class InvalidPatientIdException extends Exception {
+
+	public InvalidPatientIdException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPatientIdException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPatientIdException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPatientIdException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPatientIdException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
